039c42af8e1144465febe761fdc6b0a4610202b4
qustavoleite <qustavoleite@gmail.com>
2025-02-05 20:35:01 -0300

    feat: add function for delete tasks

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,23 @@ export function App() {
     },
   ])
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted }
+      }
+
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
+  function onDeleteClick(taskId) {
+    const newTasks = tasks.filter(task => task.id !== taskId)
+    setTasks(newTasks)
+  }
+
   return (
     <div className='w-screen h-screen bg-slate-500 flex justify-center p-6'>
       <div className='w-[500px] '>
@@ -31,7 +48,11 @@ export function App() {
           Gerenciador de tarefas
         </h1>
         {/* <AddTask /> */}
-        <Tasks tasks={tasks} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteClick={onDeleteClick}
+        />
       </div>
     </div>
   )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,12 +1,29 @@
+import { ChevronRightIcon, Trash } from 'lucide-react'
+
 export function Tasks(props) {
   return (
     <ul className='space-y-4 mt-4 p-6 bg-slate-200 rounded-md shadow'>
       {props.tasks.map((task) => (
-        <li key={task.id} className='flex'>
-          <button className='bg-slate-400 w-full text-white p-2 rounded-md'>
+        <li key={task.id} className='flex gap-2'>
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 w-full text-white p-2 rounded-md text-left ${
+              task.isCompleted && 'line-through'
+            }`}
+          >
             {task.title}
           </button>
-          <button>Ver detalhes</button>
+
+          <button className='bg-slate-400 p-2 rounded-md'>
+            <ChevronRightIcon />
+          </button>
+
+          <button
+            className='bg-slate-400 p-2 rounded-md'
+            onClick={() => props.onDeleteClick(task.id)}
+          >
+            <Trash />
+          </button>
         </li>
       ))}
     </ul>
